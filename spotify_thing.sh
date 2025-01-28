6a66d78a7955dda4c34d6d0c5a28c264b22ddc64
Gustavo Rodrigues <gustardn@gmail.com>
2025-01-27 21:20:45 -0300

    Day 2 of SpotifyThing.

--- spotify_thing.sh
+++ spotify_thing.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-response=$(curl -s "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
-     -H "Authorization: Bearer BQAXm0Bx-X_Xt2DtlTxTcQNQ2h1y1JY_P4EgN0PFyZGIw5j9IH1XHYO-Iufk85HEnRxl-vlistsBr1Z44_cWdBCsk5MyOK3Q42iYUG5xhmfbeBizhMo"
-)
+response=$(curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=d2493183072a47da84be16790faa88d9&client_secret=215d15e458e946ac85004e98d245da41")
 
 echo "$response"
-
